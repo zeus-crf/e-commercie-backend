8acedefe2ceeb277f8edf5209749bd549599cb83
Miguel Lopes <mpmiguel2007@gmail.com>
2026-07-29 18:07:53 -0300

    feat: Inicialização do repo

---
+++ src/main/java/com/ecommercie/EcommercieApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommercie/ecommercie/EcommercieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercie.ecommercie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
