4f415b9785ee097626837deb52ab1653dcce4a5b
Miguel Lopes <mpmiguel2007@gmail.com>
2026-07-30 18:24:34 -0300

    feat(security): Criação do respository do Usuarios

--- src/main/java/com/ecommercie/security/repository/UserRepository.java
+++ src/main/java/com/ecommercie/security/repository/UserRepository.java
@@ -1,7 +1,11 @@
-package com.ecommercie.security;
+package com.ecommercie.security.repository;
 
 import com.ecommercie.security.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
 }
