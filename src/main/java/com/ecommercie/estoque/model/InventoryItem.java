21c43407931b682f387f107739757f2de0b9da86
Miguel Lopes <mpmiguel2007@gmail.com>
2026-08-04 15:48:46 -0300

    feat(estoque): Criação da Entidade de InventoryItem e ligação com Product

---
+++ src/main/java/com/ecommercie/estoque/model/InventoryItem.java
@@ -0,0 +1,28 @@
+package com.ecommercie.estoque.model;
+
+import com.ecommercie.catalogo.models.Product;
+import com.ecommercie.shared.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory_item")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryItem extends BaseEntity {
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false, unique = true)
+    private Product product;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private int disponivel = 0;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private int reservada = 0;
+}
