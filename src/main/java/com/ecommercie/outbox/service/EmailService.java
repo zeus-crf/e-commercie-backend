1cd027d1756095effbcf3d2909381603f7290b73
Miguel Lopes <mpmiguel2007@gmail.com>
2026-08-12 15:19:17 -0300

    feat(outbox): Criação do service de envio de emails

---
+++ src/main/java/com/ecommercie/outbox/service/EmailService.java
@@ -0,0 +1,60 @@
+package com.ecommercie.outbox.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${app.mail.from}")
+    private String from;
+
+
+    public void enviarConfirmacaoPedido(String orderId, String destino) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(destino);
+        message.setSubject("Pagamento Confirmado - pedido " + orderId);
+        message.setText("Seu pagamento foi confirmado! Pedido: " + orderId);
+        mailSender.send(message);
+
+    }
+
+    public void enviarConfirmacaoEnviado(String orderId, String destino) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(destino);
+        message.setSubject("Pedido enviado com sucesso! Pedido - " + orderId);
+        message.setText("Seu pedido foi enviado com sucesso para " + destino);
+        mailSender.send(message);
+    }
+
+    public void enviarConfirmacaoCancelado(String orderId, String destino) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(destino);
+        message.setSubject("Pedido cancelado com sucesso! Pedido - " + orderId);
+        message.setText("Seu pedido foi cancelado");
+        mailSender.send(message);
+    }
+
+    public void enviarConfirmacaoEntregue(String orderId, String destino) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(destino);
+        message.setSubject("Pedido entregue com sucesso! Pedido - " + orderId);
+        message.setText("Seu pedido foi entregue no destino " + destino);
+        mailSender.send(message);
+    }
+
+
+
+
+
+}
