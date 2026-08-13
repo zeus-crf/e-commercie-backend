e966cec4c84f10fa76c0edf4cb2b2a056797a753
Miguel Lopes <mpmiguel2007@gmail.com>
2026-08-13 18:32:33 -0300

    feat(mp-pagamento): Criação de interface para testes

--- src/main/java/com/ecommercie/mercado_pago/PaymentGateway.java
+++ src/main/java/com/ecommercie/mercado_pago/PaymentGateway.java
@@ -1,9 +1,12 @@
 package com.ecommercie.mercado_pago;
 
 import com.ecommercie.mercado_pago.dtos.PaymentPreference;
+import com.ecommercie.mercado_pago.dtos.RequestPreference;
 import com.ecommercie.pedido.models.Order;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
 
 public interface PaymentGateway {
 
-    PaymentPreference criarPreferencia(Order order);
+    PaymentPreference criarPreferencia(Order order, RequestPreference request) throws MPException, MPApiException;
 }
