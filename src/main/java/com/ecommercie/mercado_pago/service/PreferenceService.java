596863950734018313773be34deca59885aa7b5d
Miguel Lopes <mpmiguel2007@gmail.com>
2026-08-14 10:13:44 -0300

    fix(mp-pagamento): Ajuste para usar a nova interface

--- src/main/java/com/ecommercie/mercado_pago/service/PreferenceService.java
+++ src/main/java/com/ecommercie/mercado_pago/service/PreferenceService.java
@@ -1,12 +1,10 @@
 package com.ecommercie.mercado_pago.service;
 
-import com.ecommercie.carrinho.model.Cart;
-import com.ecommercie.carrinho.repository.CartRepository;
-import com.ecommercie.catalogo.models.Product;
-import com.ecommercie.catalogo.repository.ProductRepository;
-import com.ecommercie.mercado_pago.client.MercadoPagoClient;
+import com.ecommercie.mercado_pago.PaymentGateway;
+import com.ecommercie.mercado_pago.dtos.PaymentPreference;
 import com.ecommercie.mercado_pago.dtos.RequestPreference;
-import com.ecommercie.mercado_pago.dtos.ResponsePreference;
+import com.ecommercie.pedido.models.Order;
+import com.ecommercie.pedido.repository.OrderRepository;
 import com.ecommercie.security.models.User;
 import com.ecommercie.security.repository.UserRepository;
 import com.mercadopago.exceptions.MPApiException;
@@ -14,62 +12,32 @@ import com.mercadopago.exceptions.MPException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class PreferenceService {
 
-    private final MercadoPagoClient mercadoPagoClient;
-
-
+    private final PaymentGateway paymentGateway;
     private final UserRepository userRepository;
-    private final ProductRepository productRepository;
-    private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
 
+    public PaymentPreference criarPreferencia(RequestPreference request, String orderId) throws MPException, MPApiException {
+        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
 
-    public ResponsePreference createdPreference(RequestPreference request, String orderId) throws MPException {
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
 
-        User user = null;
-        Cart carrinho = null;
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new EntityNotFoundException("Pedido não encontrado"));
 
-        if (request.userId() != null) {
-
-            user = userRepository.findById(request.userId())
-                    .orElseThrow(() ->
-                            new EntityNotFoundException("Usuário não encontrado"));
-
-            carrinho = cartRepository.findByUserId(request.userId())
-                    .orElseThrow(() ->
-                            new EntityNotFoundException("Carrinho não encontrado"));
+        if (!order.getUser().getId().equals(user.getId())) {
+            throw new AccessDeniedException("Pedido não pertence ao usuário");
         }
 
-
-
-
-        for (RequestPreference.ItemDto itemDto : request.itemsDto()) {
-            var product = productRepository.findById(itemDto.id())
-                    .orElseThrow(() -> new EntityNotFoundException("Esse produto não exite"));
-        }
-
-        try {
-            ResponsePreference response = mercadoPagoClient.createPreference(request, orderId);
-        } catch (MPApiException ex) {
-            log.error("Erro ao criar preferência na API do mercado pago: {}", ex.getMessage());
-        }
-        catch (MPException ex) {
-            log.error("Erro ao criar preferência no Mercado Pago: {}", ex.getMessage());
-            throw ex;
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return null;
+        return paymentGateway.criarPreferencia(order, request);
     }
-
 }
