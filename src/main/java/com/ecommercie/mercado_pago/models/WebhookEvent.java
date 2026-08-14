d73173783555cc6949566bca36c00d31e18bebee
Miguel Lopes <mpmiguel2007@gmail.com>
2026-08-14 16:35:41 -0300

    feat(mp-pagamento): Criação da entidade de Webhook

---
+++ src/main/java/com/ecommercie/mercado_pago/models/WebhookEvent.java
@@ -0,0 +1,14 @@
+package com.ecommercie.mercado_pago.models;
+
+import com.ecommercie.shared.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "webhook_event")
+public class WebhookEvent extends BaseEntity {
+
+    private String provider;
+    private String externalId;
+    private String status;
+}
