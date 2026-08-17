dac605324412a99946985decb8cfc8561f84f8f5
Miguel Lopes <mpmiguel2007@gmail.com>
2026-08-17 15:52:34 -0300

    fix: Adicionado o builder

--- src/main/java/com/ecommercie/mercado_pago/models/WebhookEvent.java
+++ src/main/java/com/ecommercie/mercado_pago/models/WebhookEvent.java
@@ -3,9 +3,11 @@ package com.ecommercie.mercado_pago.models;
 import com.ecommercie.shared.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Builder;
 
 @Entity
 @Table(name = "webhook_event")
+@Builder
 public class WebhookEvent extends BaseEntity {
     private String provider;
     private String externalId;
